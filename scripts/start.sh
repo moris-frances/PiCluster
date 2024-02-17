f7f4d12f7b7f7e4844ebb5aed747fbe608b923a4
moris-frances <morisfrances2@gmail.com>
2024-02-17 15:46:47 +0100

    jenkins fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,3 +2,5 @@
 
 DESTINATION="/home/morisfrances/BachelorProject/Software"
 nohup python3 $DESTINATION/DHT11.py > $DESTINATION/log.txt & echo $! > $DESTINATION/pid
+
+# nohup python3 $DESTINATION/DHT11.py > $DESTINATION/log.txt 2>&1 & echo $! > $DESTINATION/pid

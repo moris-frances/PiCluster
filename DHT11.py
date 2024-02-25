6846423d85fe2d8191f8af925b8ac4106dfed445
moris-frances <morisfrances2@gmail.com>
2024-02-25 22:25:57 +0100

    Jenkinsfile fix

--- DHT11.py
+++ DHT11.py
@@ -11,7 +11,8 @@ while True:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
-        print("Date: {s} Temp: {:.1f} F / {:.1f} C Luftfeuchtigkeit: {}%".format(date.today(), temperature_f, temperature_c, humidity))
+        print(date.today())
+        print("Temp: {:.1f} F / {:.1f} C Luftfeuchtigkeit: {}%".format(temperature_f, temperature_c, humidity))
     except RuntimeError as error:
         print(error.args[0])
         time.sleep(2.0)

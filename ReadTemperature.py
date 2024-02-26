549794015b75a2f50b84492000820a4f434b3565
moris-frances <morisfrances2@gmail.com>
2024-02-26 15:16:23 +0100

    test

--- ReadTemperature.py
+++ ReadTemperature.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import board
 import adafruit_dht
@@ -6,12 +7,18 @@ from datetime import datetime
 
 dhtDevice = adafruit_dht.DHT11(board.D23)
 
+# Getting the first command line argument as the directory path
+directory_path = sys.argv[1] if len(sys.argv) > 1 else ""
+# Ensure there is a trailing slash in the directory path
+if directory_path and not directory_path.endswith("/"):
+    directory_path += "/"
+
 while True:
     try:
         temperature_c = dhtDevice.temperature
         if temperature_c is not None:
             # Writing the temperature value to a file
-            with open("tempValue.txt", "w") as file:
+            with open(f"{directory_path}tempValue.txt", "w") as file:
                 file.write(f"{temperature_c}\n")
     except RuntimeError as error:
         print(error.args[0])

a573c190c81eca43d596955c75d62c597aafae6f
moris-frances <morisfrances2@gmail.com>
2024-02-26 15:04:08 +0100

    test

--- BenchMarkScript.py
+++ BenchMarkScript.py
@@ -1,34 +1,31 @@
 from mpi4py import MPI
 import time
 import sys
-import board
-import adafruit_dht
  
-def calculate_primes(start, end, all_temperature_humidity):
+def calculate_primes(start, end, all_temperature):
     # Dummy operation using temperature and humidity
-    for temperature_humidity in all_temperature_humidity:
-        dummy_value = (temperature_humidity[0] + temperature_humidity[1]) % 10
+    for temperature in all_temperature:
+        dummy_value = (temperature) % 10
     primes = []
     for num in range(start, end + 1):
         if all(num % i != 0 for i in range(2, int(num**0.5) + 1)):
             primes.append(num)
     return primes
 
-def read_sensor():
-    dht_device = adafruit_dht.DHT11(board.D23)
-    while True:
-        try:
-            temperature_c = dht_device.temperature
-            humidity = dht_device.humidity
-            dht_device.exit()
-            break
-        except RuntimeError as error:
-            print(error.args[0])
-            continue
-        except Exception as error:
-            dht_device.exit()
-            raise error
-    return temperature_c, humidity
+def get_sensor_readings():
+    try:
+        with open("tempValue.txt", "r") as file:
+            value = file.read().strip()  # Read the content and strip any whitespace
+            return int(value)  # Convert the string to an integer and return it
+    except FileNotFoundError:
+        print("File not found.")
+        return None
+    except ValueError:
+        print("Invalid value in file.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 def main():
     comm = MPI.COMM_WORLD
@@ -37,10 +34,10 @@ def main():
     size = comm.Get_size()
     
     # Poll every device for temperature and humidity values
-    temp_humidity = read_sensor()
-    all_temp_humidity = comm.gather(temp_humidity, root=0)  
-    print("Gatered themperature and Humidity: ")
-    print(all_temp_humidity)
+    temperature = get_sensor_readings()
+    all_temperature = comm.gather(temperature, root=0)  
+    print("Gatered themperature: ")
+    print(all_temperature)
 
     start = 2
     end = 300000 

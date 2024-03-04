ccc0409684296da209e54087d365a9463d12de25
moris-frances <morisfrances2@gmail.com>
2024-03-04 11:01:07 +0100

    added shared drive

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DESTINATION="/home/morisfrances/BachelorProject/Software"
+NFS_DIR="/nfsDir/"
 # nohup python3 -u $DESTINATION/ReadTemperature.py > $DESTINATION/log.txt 2>&1 & echo $! > $DESTINATION/pid
-nohup python3 -u $DESTINATION/ReadTemperature.py $DESTINATION > $DESTINATION/log.txt 2>&1 & echo $! > $DESTINATION/pid
+nohup python3 -u $DESTINATION/ReadTemperature.py $NFS_DIR > $DESTINATION/log.txt 2>&1 & echo $! > $DESTINATION/pid

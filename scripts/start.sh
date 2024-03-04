f5257f2a157bf0106d154077d7f60c641f1cf8ce
moris-frances <morisfrances2@gmail.com>
2024-03-04 18:19:27 +0100

    refined pipeline

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DESTINATION=$(cat ../config.json | jq -r ".destinationFolder")
-NFS_DIR=$(cat config.json | jq -r ".nfs_dir")
+NFS_DIR=$(cat ../config.json | jq -r ".nfs_dir")
 
 # nohup python3 -u $DESTINATION/ReadTemperature.py > $DESTINATION/log.txt 2>&1 & echo $! > $DESTINATION/pid
 nohup python3 -u $DESTINATION/ReadTemperature.py $NFS_DIR > $DESTINATION/log.txt 2>&1 & echo $! > $DESTINATION/pid
